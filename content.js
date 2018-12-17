044c843466a594460d208e82bc05b2f286fdd59c
Derek Chan <derekchan7@gmail.com>
2018-12-16 23:45:41 -0500

    Fix bug where empty input would throw an error; add comments

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
+// Listens for user input from the extension then makes every matching Element
+// clickable, displaying a tooltip containing its attributes
 chrome.runtime.onMessage.addListener(msg => {
-  let nodesFound = document.querySelectorAll(msg.input);
+  let nodesFound = document.querySelectorAll(msg.input); // all matching elements
   nodesFound.forEach(node => {
+    // Stylize each element so that they're easier to see
     node.className += node.className === '' ? 'dom-explore_selected' : ' dom-explore_selected';
-    node.onclick = () => {
+    node.onclick = () => { // Create tooltips to display when clicked
       let tooltipText = '';
       for(attr of node.attributes) {
         tooltipText += `${attr.name}: ${attr.value}<br>`;
@@ -10,7 +13,7 @@ chrome.runtime.onMessage.addListener(msg => {
       event.target.insertAdjacentHTML(
         'beforebegin',
         `<span class="dom-explore_tooltip">${tooltipText}</span>`);
-      event.preventDefault();
+      event.preventDefault(); // Stops links from directing, etc.
     };
   });
 });
--- popup.js
+++ popup.js
@@ -1,8 +1,11 @@
 let searchButton = document.getElementById('dom-explore_search-button');
 
+// Sends value from the search input field to the content script
 searchButton.onclick = () => {
   let searchInput = document.getElementById('dom-explore_search-input').value;
-  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    chrome.tabs.sendMessage(tabs[0].id, {input: searchInput});
-  });
+  if(searchInput !== '') {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, {input: searchInput});
+    });
+  }
 };

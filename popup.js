b36f7d46548ad24ea080681d9ac1cae6a1f1d719
Derek Chan <derekchan7@gmail.com>
2018-12-17 02:29:08 -0500

    Add label, autofocus, and onsubmit to extension

--- popup.js
+++ popup.js
@@ -1,11 +1,12 @@
+let searchInput = document.getElementById('dom-explore_search-input');
 let searchButton = document.getElementById('dom-explore_search-button');
 
 // Sends value from the search input field to the content script
-searchButton.onclick = () => {
-  let searchInput = document.getElementById('dom-explore_search-input').value;
-  if(searchInput !== '') {
+searchInput.onsubmit = searchButton.onclick = () => {
+  let inputValue = searchInput.value;
+  if(inputValue !== '') {
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-      chrome.tabs.sendMessage(tabs[0].id, {input: searchInput});
+      chrome.tabs.sendMessage(tabs[0].id, {input: inputValue});
     });
   }
 };

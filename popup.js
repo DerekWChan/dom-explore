f6d116e10c352dfd37562de968072b6ed3b6c8cf
Derek Chan <derekchan7@gmail.com>
2018-12-16 21:29:37 -0500

    Implement basic search bar which sends user input to active tab

--- popup.js
+++ popup.js
@@ -1,15 +1,8 @@
-let changeColor = document.getElementById('changeColor');
+let searchButton = document.getElementById('search-button');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+searchButton.onclick = () => {
+  let searchInput = document.getElementById('search-input').value;
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {input: searchInput});
   });
 };

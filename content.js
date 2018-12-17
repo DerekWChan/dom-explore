0cc1a5b1476c1f1f0212a970fda36beb7f3f42ab
Derek Chan <derekchan7@gmail.com>
2018-12-16 23:22:44 -0500

    Implement basic tooltip popups with element attributes

--- content.js
+++ content.js
@@ -1,7 +1,16 @@
-chrome.runtime.onMessage.addListener((msg) => {
+chrome.runtime.onMessage.addListener(msg => {
   let nodesFound = document.querySelectorAll(msg.input);
-  nodesFound.forEach((node) => {
-    node.className += ' selected';
-    node.onclick = () => console.log(node);
+  nodesFound.forEach(node => {
+    node.className += node.className === '' ? 'dom-explore_selected' : ' dom-explore_selected';
+    node.onclick = () => {
+      let tooltipText = '';
+      for(attr of node.attributes) {
+        tooltipText += `${attr.name}: ${attr.value}<br>`;
+      }
+      event.target.insertAdjacentHTML(
+        'beforebegin',
+        `<span class="dom-explore_tooltip">${tooltipText}</span>`);
+      event.preventDefault();
+    };
   });
 });
--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-let searchButton = document.getElementById('search-button');
+let searchButton = document.getElementById('dom-explore_search-button');
 
 searchButton.onclick = () => {
-  let searchInput = document.getElementById('search-input').value;
-  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+  let searchInput = document.getElementById('dom-explore_search-input').value;
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     chrome.tabs.sendMessage(tabs[0].id, {input: searchInput});
   });
 };
